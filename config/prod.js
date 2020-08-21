cf6822dceaba3f3ca54c783e9d91176032fb4ff1
Pijus <user@users-MBP.lan>
2020-08-21 14:45:30 +0300

    testing mongodb

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
+  mongoURI: "mongodb+srv://Pijus:Vaidilute123.@cluster0.bpmas.mongodb.net/surveys-prod?retryWrites=true&w=majority",
   cookieKey: process.env.COOKIE_KEY,
 };
