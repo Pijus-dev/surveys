c3e43bf3c55a4cfaa30b0036b012a8fed5fa8e28
Pijus <user@users-MBP.lan>
2020-08-21 14:49:50 +0300

    testing mongodb with heroku

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: "mongodb+srv://Pijus:Vaidilute123.@cluster0.bpmas.mongodb.net/surveys-prod?retryWrites=true&w=majority",
+  mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
