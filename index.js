28dc37d7f0d89ed687e5ac1a179f98ef4344d9e0
Pijus <user@users-MBP.lan>
2020-08-21 11:43:46 +0300

    finish oauth flow

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require("express");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+require("./models/user");
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "body" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT);
---
+++ services/passport.js
@@ -0,0 +1,39 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
+const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      // creating an user instance in the mongoDB
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (!existingUser) {
+        const user = await new User({
+          googleId: profile.id,
+          name: profile.displayName,
+        }).save();
+        done(null, user);
+      }
+
+      done(null, existingUser);
+    }
+  )
+);
