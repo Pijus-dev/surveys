6dc6f6421dc80a2dfdc6c931ad0fb410b1e193d7
Pijus <user@users-MBP.lan>
2020-08-21 14:32:34 +0300

    testing heroku prod mode

--- config/prod.js
+++ config/prod.js
@@ -1,9 +1,6 @@
 module.exports = {
-  //   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientID:
-    "981023189889-j4bao2k6b28mq5u4vuatbm15gtljbdk1.apps.googleusercontent.com",
-  //   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  googleClientSecret: "B-DmzryYfxWftw3fjvvDpSz7",
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
