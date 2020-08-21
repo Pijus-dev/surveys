1e1e6af849c6d9c0b9a0794f60f897c78c53b841
Pijus <user@users-MBP.lan>
2020-08-21 14:27:55 +0300

    testing prod mode

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,9 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  //   googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientID:
+    "981023189889-j4bao2k6b28mq5u4vuatbm15gtljbdk1.apps.googleusercontent.com",
+  //   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientSecret: "B-DmzryYfxWftw3fjvvDpSz7",
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
 };
