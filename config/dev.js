3539e8e3d8878cadcc8586a5b625cd6057d86424
Pijus <user@users-MBP.lan>
2020-08-24 16:47:33 +0300

    removing gitignore files

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    "904087232794-mj8r46820irc2cuu137mimrdtriiga8d.apps.googleusercontent.com",
+  googleClientSecret: "7IZHhdANenL0_I7Ia6-tcTIy",
+  mongoURI:
+    "mongodb+srv://Pijus:Vaidilute123.@cluster0.m9ull.mongodb.net/surveys-dev?retryWrites=true&w=majority",
+  cookieKey: "435nbjwfe9843",
+};
